2b25359a23497ed9188e6d382778f727a4872edc
Daniel <danya.smile@gmail.com>
2024-06-11 12:00:05 +0300

    Css minor fixes

--- dk_personal_site/src/components/Footer.tsx
+++ dk_personal_site/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 const Footer = () => {
     return(
         <>
-            <footer className="header_wrapper">
+            <footer className="footer_wrapper">
                 <div className="header_content">
                     <p className="subsub"><span className="green_p">[ contact me ]</span></p>
                     <h2 className="titlefoot" id="im">kovalenko.daniel.it@gmail.com</h2>
+                    <h2 className="subsub" id="im">053-879-16-14</h2>
                     <h3 className="standard_text">{String.fromCharCode(169)} 2024</h3>
                 </div>
             </footer>
